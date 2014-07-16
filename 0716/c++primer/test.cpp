d2f9a9846fb46547d70d1c3a0c5cd1f27b4b1252
liubin <xujie@ubuntu.(none)>
2014-07-16 22:07:29 +0800

    Initial commit with contributors

---
+++ 0716/c++primer/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void f(const vector<int> &vec)
+{
+    
+}
+int main(int argc, const char *argv[])
+{
+    vector<int> v2;
+    f(v2);
+    f(42);
+    return 0;
+}
